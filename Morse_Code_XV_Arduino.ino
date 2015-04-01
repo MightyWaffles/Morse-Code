9dc6139ccfbc478e917e2a7e50f0be1bdb486725
MightyWaffles <piperlikespie@gmail.com>
2015-04-01 10:05:48 -0700

    Create Morse_Code_XV_Arduino.ino
    creation

---
+++ Morse_Code_XV_Arduino.ino
@@ -0,0 +1,18 @@
+#include <morse.h>
+
+morse DoStuff;
+
+void setup()
+{
+  //This does stuff
+  DoStuff.AndStuff(13);
+}
+
+void loop()
+{
+  //And this does things
+  DoStuff.AndThings();
+}
+
+
+//And this makes you feel like an idiot
